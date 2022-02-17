9b9867a998ef98c3fbb48a86fa5e4543f8f31179
Renan F. Stein <renan_stein@hotmail.com>
2022-02-17 17:07:36 -0300

    Finalizado a introdução, agora vou modificando e incluindo mais validações/opões

--- usuario/views.py
+++ usuario/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Usuario
 
 def cadastro(request):
     return HttpResponse('Ola')
@@ -8,4 +9,17 @@ def login(request):
     return render(request, 'login.html')
 
 def home(request):
-    return HttpResponse('Você esta na pagina principal')
+    return HttpResponse( 'Você esta na pagina principal')
+
+def salvar(request):
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    idade = request.POST.get('idade')
+    usuario = Usuario(
+        nome = nome,
+        sobrenome = sobrenome,
+        idade = idade
+    )
+    usuario.save()
+    return HttpResponse('Teste')
+
